8349a309f587debfd632cfabdc2d9f73d8c996de
Stano, Tomasz <tomasz.stano@aptiv.com>
2022-03-24 09:24:11 +0100

    Mod 1.3 Prime Factors - part 3 / first step - add UT cases

--- module_1_3_TDD_prime_factors.py
+++ module_1_3_TDD_prime_factors.py
@@ -1,9 +1,21 @@
 """Module 1.3 - Zadanie: faktoryzacja"""
 
+from typing import Union
 
-def prime_factors(number):
-    raise NotImplementedError("TODO")
+
+def prime_factors(number: int) -> Union[list, list[int]]:
+    """
+    Return prime factor list for a given number.
+    Assume that `number` is int.
+    Raise ValueError `number` is not int.
+    If number equal 1 return empty list.
+    """
+
+    if not isinstance(number, int):
+        raise ValueError("number must be int!")
+
+    pass
 
 
 if __name__ == "__main__":
-    prime_factors(number=3958159172)
+    print(prime_factors(number=3958159172))
--- test_module_1_3_TDD_prime_factors.py
+++ test_module_1_3_TDD_prime_factors.py
@@ -5,7 +5,7 @@ from module_1_3_TDD_prime_factors import prime_factors
 
 
 class TestPrimeFactor(unittest.TestCase):
-    """ Class for test get prime factor """
+    """Class for test prime_factor function."""
 
     def test_import_prime_factors(self):
         try:
@@ -14,9 +14,32 @@ class TestPrimeFactor(unittest.TestCase):
         except ImportError as err:
             assert False, err
 
-    def test_pf_not_implemented_error(self):
-        with self.assertRaises(NotImplementedError):
-            prime_factors(number=1)
+    def test_prime_factor_number_is_int(self):
+        with self.assertRaises(ValueError) as exc:
+            prime_factors(number='test')
+
+        with self.assertRaises(ValueError):
+            prime_factors(number=0.1)
+
+    def test_prime_factor_value_err_msg(self):
+        try:
+            prime_factors(number='test')
+        except Exception as exc:
+            self.assertEqual(str(exc), "number must be int!")
+
+    def test_prime_factor_number_set_as_1_return_empty_list(self):
+        self.assertEqual(prime_factors(1), [])
+
+    def test_prime_factor_kodilla_case(self):
+        self.assertEqual(prime_factors(number=3958159172), [2, 2, 11, 2347, 38329])
+
+    def test_get_multiple_call_prime_factor_with_different_numbers(self):
+        self.assertEqual(prime_factors(2), [2])
+        self.assertEqual(prime_factors(6), [2, 3])
+        self.assertEqual(prime_factors(7), [7])
+        self.assertEqual(prime_factors(8), [2, 2, 2])
+        self.assertEqual(prime_factors(9), [3, 3])
+        self.assertEqual(prime_factors(17), [17])
 
 
 if __name__ == '__main__':
